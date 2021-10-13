0486bcad0a0520b648de8af2e5f0e5b97800c5a7
Dmitry Gorohov <dgorohov@izettle.com>
2021-10-13 15:19:49 +0200

    Update

--- Formula/rsuuid.rb
+++ Formula/rsuuid.rb
@@ -1,7 +1,7 @@
 class Rsuuid < Formula
   desc "UUID CLI generator"
   homepage "https://github.com/dgorohov/rsuuid"
-  url "https://github.com/dgorohov/homebrew-home.git"
+  url "https://github.com/dgorohov/rsuuid/archive/v0.1.2.tar.gz"
   sha256 "18925d551efb897e788d11f8e4ffac0e02c6c64c7f21aac1b8635ab7f38748a5"
   license "Apache-2.0"
 
