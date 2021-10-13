c173565e25288929e1574b90f8ca3f126f2ff214
Dmitry Gorohov <dgorohov@izettle.com>
2021-10-13 12:58:02 +0200

    Removed version tag

--- Formula/rsuuid.rb
+++ Formula/rsuuid.rb
@@ -4,7 +4,6 @@ class Rsuuid < Formula
   url "https://github.com/dgorohov/rsuuid/archive/v0.1.2.tar.gz"
   sha256 "18925d551efb897e788d11f8e4ffac0e02c6c64c7f21aac1b8635ab7f38748a5"
   license "Apache-2.0"
-  version = "v0.1.0"
 
   depends_on "rust" => :build
 
