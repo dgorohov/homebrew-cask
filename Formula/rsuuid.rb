d5fae2dca7e843abe8fc8234d5301fc682974291
Dmitry Gorohov <dgorohov@izettle.com>
2021-10-13 08:54:33 +0200

    Updated formula

--- Formula/rsuuid.rb
+++ Formula/rsuuid.rb
@@ -1,12 +1,14 @@
 class Rsuuid < Formula
   desc "UUID CLI generator"
-  homepage ""
-  url "https://github.com/dgorohov/rsuuid/releases/download/v0.1.0/rsuuid-macos-amd64"
-  sha256 "79d6ead82b34c2ecc15e5565a7a52e06810f061d9f264cbc8108b52914c87fa9"
+  homepage "https://github.com/dgorohov/rsuuid"
+  url "https://github.com/dgorohov/rsuuid/archive/refs/tags/v0.1.0.zip"
+  sha256 "74803748725d0d4236d87a4ff6bd85ecd1ac0921a57927a3e262c258cd1a5597"
   license "Apache-2.0"
 
+  depends_on "rust" => :build
+
   def install
-    bin.install "rsuuid"
+    system "cargo", "install", "--bin", "rsuuid", "--release", "--locked", "--root", prefix.to_s, "--path", "."
   end
 
 end
